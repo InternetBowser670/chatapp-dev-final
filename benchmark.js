2f55279cbf666e81b5aa0314bb97a9c8e81d6cd9
Joshua Ghattas <102930366+InternetBowser670@users.noreply.github.com>
2024-09-11 11:45:50 +0000

    I forgot

--- benchmark.js
+++ benchmark.js
@@ -1,3 +1,13 @@
 benchmark = require("speedybenchmark")
 
 benchmark.blackjack(1000000)
+
+const express = require("express")
+
+app = express()
+
+app.get("/", (req, res) => {
+  res.end("One sec")
+})
+
+app.listen(3000)
